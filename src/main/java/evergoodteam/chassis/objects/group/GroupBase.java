01f4a83ea82bb64feb3b2cebea333d0b4e6d186b
Libra <david.popusoi2004@gmail.com>
2022-03-25 19:52:36 +0100

    25/03 19:52

--- src/main/java/evergoodteam/chassis/objects/group/GroupBase.java
+++ src/main/java/evergoodteam/chassis/objects/group/GroupBase.java
@@ -1,14 +1,14 @@
 package evergoodteam.chassis.objects.group;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class GroupBase {
 
-    public static ItemGroup createItemGroup(String namespace, String path, Item icon){
+    public static ItemGroup createItemGroup(String namespace, String path, ItemConvertible icon){
 
         ItemGroup result = FabricItemGroupBuilder.build(new Identifier(namespace, path),
                 () -> new ItemStack(icon));
