a794906280524decd1da21be2b3fa3c1813a6c87
Libra <david.popusoi2004@gmail.com>
2022-04-28 21:26:41 +0200

    28/04 21:26

--- src/main/java/evergoodteam/chassis/objects/assets/LangJson.java
+++ src/main/java/evergoodteam/chassis/objects/assets/LangJson.java
@@ -1,31 +1,17 @@
 package evergoodteam.chassis.objects.assets;
 
 import com.google.gson.JsonObject;
-import net.minecraft.util.Identifier;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class LangJson {
 
-    public static JsonObject createLangJson(Map<String, String> entries) {
-
-        List<Identifier> identifiers = new ArrayList<>();
-
-        for (Map.Entry<String, String> entry : entries.entrySet()) {
-            identifiers.add(new Identifier(entry.getKey(), entry.getValue()));
-        }
-
-        return createLangJson(identifiers);
-    }
-
-    public static JsonObject createLangJson(List<Identifier> entries) {
+    public static JsonObject createLangJson(Map<String, String> entries){
 
         JsonObject json = new JsonObject();
 
-        for (Identifier entry : entries) {
-            json.addProperty(entry.getNamespace(), entry.getPath());
+        for(int i = 0; i < entries.keySet().size(); i++){
+            json.addProperty(entries.keySet().toArray()[i].toString(), entries.get(entries.keySet().toArray()[i]));
         }
 
         return json;
