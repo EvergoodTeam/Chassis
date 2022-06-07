6cadd55956080b36a4b4a5981e8f1f97ed83c658
Libra <david.popusoi2004@gmail.com>
2022-06-07 19:51:46 +0200

    Recomment features

--- src/main/java/evergoodteam/chassis/Chassis.java
+++ src/main/java/evergoodteam/chassis/Chassis.java
@@ -7,7 +7,8 @@ import evergoodteam.chassis.objects.blocks.BlockBase;
 import evergoodteam.chassis.objects.groups.ItemGroupBase;
 import evergoodteam.chassis.objects.items.ItemBase;
 import evergoodteam.chassis.util.handlers.InjectionHandler;
-import evergoodteam.chassis.util.handlers.RegistryHandler;*/
+import evergoodteam.chassis.util.handlers.RegistryHandler;
+*/
 import net.fabricmc.api.ModInitializer;
 /*
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -17,15 +18,15 @@ import net.minecraft.block.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;*/
-
+import net.minecraft.util.Identifier;
+*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 /*
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;*/
-
+import java.util.List;
+*/
 import static evergoodteam.chassis.util.Reference.*;
 
 public class Chassis implements ModInitializer {
@@ -62,15 +63,15 @@ public class Chassis implements ModInitializer {
 
         // Asset injection
         InjectionHandler.addAssetInjection(MODID);
-        InjectionHandler.addLoot(MODID, "testblock", LootJson.createBlockBreakLootJson("chassis", "testblock"));
+        InjectionHandler.addLoot(MODID, "blocks/testblock", LootJson.createBlockBreakLootJson("chassis", "testblock"));
         InjectionHandler.addRecipe(MODID, "testrecipe", RecipeJson.create3x3RecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 1));
         InjectionHandler.addRecipe(MODID, "testrecipeshapeless", RecipeJson.createShapelessRecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 9));
 
         // Resourcepack injection
         CHASSIS_RESOURCES.createGlobalTag("testblock")
                 .createBlockstate("testblock")
-                .createBlockModels("testblock", "testblock", "all")
-                .createBlockDropLootTable("testblock")
+                //.createBlockModels("testblock", "testblock", "all")
+                //.createBlockDropLootTable("testblock")
                 .createRequiredToolTag("pickaxe", new String[]{"testblock"})
                 .createMiningLevelTag("stone", new String[]{"testblock"})
                 .createTexture(true, "https://i.imgur.com/BAStRdD.png", "testblock") // Truly a legendary URL
