b1aaa0f70cb3123953d7b069b6fc1b430b273b4b
Libra <david.popusoi2004@gmail.com>
2022-06-09 15:22:08 +0200

    09/06 15:22

--- src/main/java/evergoodteam/chassis/objects/assets/LootJson.java
+++ src/main/java/evergoodteam/chassis/objects/assets/LootJson.java
@@ -2,10 +2,8 @@ package evergoodteam.chassis.objects.assets;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import lombok.extern.log4j.Log4j2;
 import net.minecraft.util.Identifier;
 
-@Log4j2
 public class LootJson {
 
     public static JsonObject createBlockBreakLootJson(String namespace, String path) {
