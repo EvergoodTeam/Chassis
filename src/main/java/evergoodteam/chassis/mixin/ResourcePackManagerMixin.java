6b923b28eeb3ea6fb03972ff371a066e36a6f16e
Libra <73041325+Librv@users.noreply.github.com>
2022-07-28 15:46:59 +0200

    Fix crash on immutable add from mixin

--- src/main/java/evergoodteam/chassis/mixin/ResourcePackManagerMixin.java
+++ src/main/java/evergoodteam/chassis/mixin/ResourcePackManagerMixin.java
@@ -1,5 +1,6 @@
 package evergoodteam.chassis.mixin;
 
+import com.google.common.collect.ImmutableSet;
 import evergoodteam.chassis.objects.resourcepacks.ClientResourcePackProvider;
 import evergoodteam.chassis.objects.resourcepacks.ResourcePackBase;
 import evergoodteam.chassis.objects.resourcepacks.ServerResourcePackProvider;
@@ -8,26 +9,37 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.resource.ResourcePackProfile;
 import net.minecraft.resource.ResourcePackProvider;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static evergoodteam.chassis.util.Reference.CMI;
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Mixin(ResourcePackManager.class)
 public class ResourcePackManagerMixin {
 
+    private static Logger LOGGER = getLogger(CMI + "/Resource");
+
     @Shadow
     @Final
+    @Mutable
     private Set<ResourcePackProvider> providers;
 
     @Inject(method = "providePackProfiles", at = @At(value = "HEAD"))
     private void injectProvidePackProfiles(CallbackInfoReturnable<Map<String, ResourcePackProfile>> cir) {
+
+        LOGGER.info("Injecting Chassis' resource providers");
 
         boolean providerAlreadyExists = false;
         boolean client;
@@ -48,6 +60,8 @@ public class ResourcePackManagerMixin {
 
         Map<String, List<ResourcePackBase>> m = ResourcePackBase.RESOURCE_PACKS;
 
+        Set<ResourcePackProvider> prov = new HashSet<>(this.providers);
+
         if (!providerAlreadyExists) {
             for (int i = 0; i < m.keySet().size(); i++) {
 
@@ -58,14 +72,16 @@ public class ResourcePackManagerMixin {
                     ResourcePackBase r = m.get(m.keySet().toArray()[i]).get(j);
 
                     if (client) {
-                        this.providers.add(new ClientResourcePackProvider(id, r.namespace, r.hexDescColor));
+                        prov.add(new ClientResourcePackProvider(id, r.namespace, r.hexDescColor));
                         //LOGGER.info("Injected our ClientProvider into providers: {}", this.providers);
                     } else {
-                        this.providers.add(new ServerResourcePackProvider(id, r.namespace));
+                        prov.add(new ServerResourcePackProvider(id, r.namespace));
                         //LOGGER.info("Injected our ServerProvider into providers: {}", this.providers);
                     }
                 }
             }
         }
+
+        this.providers = ImmutableSet.copyOf(prov);
     }
 }
