84c3795102e0cfe13ce3d8ed961cfbca983d4222
Libra <73041325+Librv@users.noreply.github.com>
2022-09-22 18:54:42 +0200

    JsonUtils.java JavaDoc, property addition if not null

--- src/main/java/evergoodteam/chassis/util/JsonUtils.java
+++ src/main/java/evergoodteam/chassis/util/JsonUtils.java
@@ -3,7 +3,9 @@ package evergoodteam.chassis.util;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.io.FileNotFoundException;
@@ -18,41 +20,66 @@ public class JsonUtils {
     private static final Logger LOGGER = getLogger(CMI + "/U/Json");
 
     /**
-     * @param file Path of a valid .json File
-     * @return {@link JsonObject} with all the contents of the specified .json File
+     * Checks whether the specified file is a .json file
      */
-    public static JsonObject getJsonObject(@NotNull Path file) {
+    public static boolean isJsonFile(Path path) {
+        return FileUtils.ofExtension(path, ".json");
+    }
 
-        JsonElement jsonElement = null;
-
+    /**
+     * Fetches the contents of a .json file and returns them as a JsonObject
+     *
+     * @param file path of a valid .json file
+     * @return {@link JsonObject} with all the contents of the specified .json file
+     */
+    public static @Nullable JsonObject toJsonObject(@NotNull Path file) {
         try {
-            jsonElement = JsonParser.parseReader(new FileReader(file.toString()));
+            JsonElement jsonElement = JsonParser.parseReader(new FileReader(file.toString()));
+            return jsonElement.getAsJsonObject();
         } catch (FileNotFoundException e) {
             LOGGER.error("File not found", e);
         }
 
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-        return jsonObject;
+        return null;
     }
 
     /**
-     * Translates String to a JsonObject
+     * Translates a string to a {@link JsonObject}
      *
-     * @param json Naturally has to be a valid Json transcript
-     * @return
+     * @param json valid json transcript
      */
-    public static JsonObject getJsonObject(@NotNull String json) {
+    public static JsonObject toJsonObject(@NotNull String json) {
         return JsonParser.parseString(json).getAsJsonObject();
     }
 
     /**
-     * Translates JsonObject to a String
+     * Translates a {@link JsonObject} to a string
      *
-     * @param jsonObject Naturally has to be a valid {@link JsonObject}
-     * @return
+     * @param json valid {@link JsonObject}
      */
-    public static String getString(@NotNull JsonObject jsonObject) {
-        return jsonObject.toString();
+    public static String toString(@NotNull JsonObject json) {
+        return json.toString();
+    }
+
+    /**
+     * Adds the specified value if not null to the provided {@link JsonObject}
+     *
+     * @param parent {@link JsonObject} to add to
+     * @param name   name of the property
+     * @param value  value of the property
+     */
+    public static void addPropertyIfNotNull(JsonObject parent, String name, Identifier value) {
+        if (value != null) parent.addProperty(name, value.toString());
+    }
+
+    /**
+     * Adds the specified value if not null to the provided {@link JsonObject}
+     *
+     * @param parent {@link JsonObject} to add to
+     * @param name   name of the property
+     * @param value  value of the property
+     */
+    public static void addPropertyIfNotNull(JsonObject parent, String name, String value) {
+        if (value != null) parent.addProperty(name, value);
     }
 }
