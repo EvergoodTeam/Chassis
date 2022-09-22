84b4afd55e4582161ca03e42d599b1bc113b3900
Libra <73041325+Librv@users.noreply.github.com>
2022-09-22 18:49:11 +0200

    Add transparent blocks through methods

--- src/main/java/evergoodteam/chassis/client/ChassisClient.java
+++ src/main/java/evergoodteam/chassis/client/ChassisClient.java
@@ -8,20 +8,36 @@ import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
 import org.slf4j.Logger;
 
-import static evergoodteam.chassis.objects.blocks.BlockBase.TRANSPARENT;
-import static evergoodteam.chassis.util.Reference.MODID;
+import java.util.ArrayList;
+import java.util.List;
+
+import static evergoodteam.chassis.util.Reference.CMI;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Environment(EnvType.CLIENT)
 public class ChassisClient implements ClientModInitializer {
 
-    private static final Logger LOGGER = getLogger(MODID + "/Client");
+    private static final List<Block> TRANSPARENT_BLOCKS = new ArrayList<>();
+    private static final Logger LOGGER = getLogger(CMI + "/Client");
 
     @Override
     public void onInitializeClient() {
+        //LOGGER.info("Total transparent blocks: {}", getTransparentBlocks().size());
 
-        for (Block block : TRANSPARENT) {
+        for (Block block : getTransparentBlocks()) {
             BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
         }
+    }
+
+    public static void addTransparentBlock(Block block) {
+        TRANSPARENT_BLOCKS.add(block);
+    }
+
+    public static void removeTransparentBlock(Block block) {
+        TRANSPARENT_BLOCKS.remove(block);
+    }
+
+    public static List<Block> getTransparentBlocks() {
+        return TRANSPARENT_BLOCKS;
     }
 }
