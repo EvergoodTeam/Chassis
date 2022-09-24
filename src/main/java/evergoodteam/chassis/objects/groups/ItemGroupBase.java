8d2d332bc177855784d6ec6ac0624c8f7ebcc127
Libra <73041325+Librv@users.noreply.github.com>
2022-09-24 14:26:09 +0200

    ItemGroupBase.java: getters, static methods for group creation, JavaDoc

--- src/main/java/evergoodteam/chassis/objects/groups/ItemGroupBase.java
+++ src/main/java/evergoodteam/chassis/objects/groups/ItemGroupBase.java
@@ -1,15 +1,21 @@
 package evergoodteam.chassis.objects.groups;
 
+import evergoodteam.chassis.objects.EntryBase;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
-public class ItemGroupBase {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemGroupBase implements EntryBase {
+
+    private static final Map<String, ItemGroupBase> GROUP_MAP = new HashMap<>();
 
     public ItemGroup group;
-    public String namespace;
+    public Identifier identifier;
     public ItemConvertible icon;
 
     /**
@@ -20,8 +26,62 @@ public class ItemGroupBase {
      * @param icon      accepts Item or Block objects; will be used as the icon for the Creative Tab
      */
     public ItemGroupBase(String namespace, String path, ItemConvertible icon) {
-        this.group = FabricItemGroupBuilder.build(new Identifier(namespace, path), () -> new ItemStack(icon));
-        this.namespace = namespace;
+        this(new Identifier(namespace, path), icon);
+    }
+
+    /**
+     * Creates an ItemGroup, a.k.a. a creative tab
+     *
+     * @param identifier ID of your addition
+     * @param icon       accepts Item or Block objects; will be used as the icon for the Creative Tab
+     */
+    public ItemGroupBase(Identifier identifier, ItemConvertible icon) {
+        this.group = FabricItemGroupBuilder.build(identifier, () -> new ItemStack(icon));
+        this.identifier = identifier;
         this.icon = icon;
+
+        GROUP_MAP.put(identifier.toString(), this);
+    }
+
+    /**
+     * Gets the ItemGroup associated with the ItemGroupBase
+     */
+    public ItemGroup getGroup() {
+        return this.group;
+    }
+
+    /**
+     * Gets the ItemGroupBase with the provided identifier
+     */
+    public static ItemGroupBase getGroup(Identifier identifier) {
+        return getGroupMap().get(identifier.toString());
+    }
+
+    /**
+     * Gets all the ItemGroupBases created
+     */
+    public static Map<String, ItemGroupBase> getGroupMap() {
+        return GROUP_MAP;
+    }
+
+    /**
+     * Creates an ItemGroup, a.k.a. a creative tab
+     *
+     * @param namespace your modid
+     * @param path      used to identify from other additions from the same namespace
+     * @param icon      accepts Item or Block objects; will be used as the icon for the Creative Tab
+     */
+    public static ItemGroup createItemGroup(String namespace, String path, ItemConvertible icon) {
+        return new ItemGroupBase(namespace, path, icon).getGroup();
+    }
+
+    /**
+     * Creates an ItemGroup, a.k.a. a creative tab
+     *
+     * @param identifier ID of your addition
+     * @param icon       accepts Item or Block objects; will be used as the icon for the Creative Tab
+     */
+    public static ItemGroup createItemGroup(Identifier identifier, ItemConvertible icon) {
+        return new ItemGroupBase(identifier, icon).getGroup();
     }
 }
