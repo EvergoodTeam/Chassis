31e241650fb84b2f62247e2083b0ab1564317057
Libra <73041325+Librv@users.noreply.github.com>
2022-09-24 15:02:16 +0200

    Update reference example code + temporary assets

--- src/main/java/evergoodteam/chassis/Chassis.java
+++ src/main/java/evergoodteam/chassis/Chassis.java
@@ -1,29 +1,35 @@
 package evergoodteam.chassis;
-/*
-import evergoodteam.chassis.configs.ConfigHandler;
+
+import evergoodteam.chassis.client.models.BlockModelType;
+import evergoodteam.chassis.client.models.ItemModelType;
+import evergoodteam.chassis.configs.options.BooleanOption;
+import evergoodteam.chassis.configs.options.DoubleSliderOption;
 import evergoodteam.chassis.objects.assets.RecipeJson;
 import evergoodteam.chassis.objects.blocks.BlockBase;
+import evergoodteam.chassis.objects.blocks.PillarBase;
 import evergoodteam.chassis.objects.groups.ItemGroupBase;
+import evergoodteam.chassis.client.models.ModelBundler;
 import evergoodteam.chassis.objects.items.ItemBase;
-import evergoodteam.chassis.util.handlers.InjectionHandler;
+import evergoodteam.chassis.objects.recipes.RecipeBundler;
 import evergoodteam.chassis.util.handlers.RegistryHandler;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-*/
-
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Set;
 
 import static evergoodteam.chassis.util.Reference.*;
 
@@ -36,52 +42,72 @@ public class Chassis implements ModInitializer {
 
         LOGGER.info("Initializing Chassis");
 
-        //estFeatures();
+        testFeatures();
     }
 
-/*
     static final List<Block> BLOCKS = new ArrayList<>();
-    static final Block TEST_BLOCK = new BlockBase(BLOCKS, FabricBlockSettings.of(Material.METAL).requiresTool().hardness(1.5f).resistance(6.0f).sounds(BlockSoundGroup.METAL).nonOpaque(), true);
-    static final Item TEST_ITEM = new ItemBase(new FabricItemSettings().maxCount(65));
+    static final List<Item> ITEMS = new ArrayList<>();
+    static final Block TEST_BLOCK = new BlockBase(BLOCKS, FabricBlockSettings.of(Material.METAL).requiresTool().hardness(1.5f).resistance(6.0f).sounds(BlockSoundGroup.METAL).nonOpaque()).setTransparent();
+    static final Item TEST_ITEM = new ItemBase(new FabricItemSettings().maxCount(65)).addToList(ITEMS);
+
+    static final Block BIRCH = new PillarBase(FabricBlockSettings.copyOf(Blocks.BIRCH_LOG));
 
     private static void testFeatures() {
 
         // Configs
-        CHASSIS_CONFIGS.addProperty("hideResourcePack", false, "Hides the Chassis ResourcePack from the GUI")
-                .addProperty("testProperty", true, "Test comment")
-                .addProperty("testProperty2", true)
-                .addProperty("testProperty3", true, "Test comment 3")
+        CHASSIS_CONFIGS.addBooleanProperty(CHASSIS_RESOURCES.getHiddenBoolean().hideDefault(false))
+                .addIntegerSliderProperty("integerSlider", 3, 8, 5, Text.literal("slider"))
+                .addStringProperty("string", "test2", Set.of("test1", "test2", "test3", "test4"), Text.literal("stringset"))
+                .addBooleanProperty(new BooleanOption("boolean", false, Text.literal("Boolean tooltip"))
+                        .setComment("Boolean comment"))
+                .addDoubleProperty(new DoubleSliderOption("double", 3, 8, 4.5, Text.literal("Double tooltip"))
+                        .setComment("Double comment"))
                 .registerProperties();
 
         // Blocks/Items
-        ItemGroup testGroup = new ItemGroupBase("chassis", "testgroup", TEST_BLOCK).group;
+        ItemGroup testGroup = ItemGroupBase.createItemGroup("chassis", "testgroup", TEST_BLOCK);
         RegistryHandler.registerBlockAndItem("chassis", "testblock", TEST_BLOCK, testGroup);
-        RegistryHandler.registerGeneratedItem("chassis", "testitem", TEST_ITEM);
+        RegistryHandler.registerHandheldItem("chassis", "testitem", TEST_ITEM);
+        RegistryHandler.registerBlockAndItem("chassis", "birch", BIRCH, testGroup);
 
         // Asset injection
-        InjectionHandler.addModelBundler(MODID);
-        InjectionHandler.addRecipe(MODID, "testrecipe", RecipeJson.create3x3RecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 1));
-        InjectionHandler.addRecipe(MODID, "testrecipeshapeless", RecipeJson.createShapelessRecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 9));
+        ModelBundler modelBundler = new ModelBundler(MODID)
+                .addItem(new Identifier(MODID, "testitem"), ItemModelType.HANDHELD)
+                .addBlock(new Identifier(MODID, "testblock"), BlockModelType.ALL)
+                .addItem(new Identifier(MODID, "testblock"), ItemModelType.BLOCK)
+                .addColumn(new Identifier(MODID, "birch"))
+                .addItem(new Identifier(MODID, "birch"), ItemModelType.BLOCK);
+
+        RecipeBundler recipeBundler = new RecipeBundler(MODID)
+                .addRecipe(MODID, "testrecipe", RecipeJson.create3x3RecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 1))
+                .addRecipe(MODID, "testrecipeshapeless", RecipeJson.createShapelessRecipeJson("item", new Identifier(MODID, "testitem"), new Identifier(MODID, "testblock"), 9));
 
         // ResourcePack
         CHASSIS_RESOURCES.createGlobalTag("testblock")
                 .createBlockstate("testblock")
                 //.createBlockModels("testblock", "testblock", "all")
-                .createBlockDropLootTable("testblock")
+                //.createBlockDropLootTable("testblock")
+                .createGemOreDropLootTable("testblock", MODID, "testitem")
                 .createRequiredToolTag("pickaxe", new String[]{"testblock"})
                 .createMiningLevelTag("stone", new String[]{"testblock"})
                 .createTexture(true, "https://i.imgur.com/BAStRdD.png", "testblock") // Truly a legendary URL
 
-                .createItemModel("testitem", "testitem")
+                //.createItemModel("testitem", "testitem")
                 .createTexture(false, "https://i.imgur.com/BAStRdD.png", "testitem")
+
+                .createBlockDropLootTable("birch")
+                .createColumnBlockstate("birch")
+                //.createBlockModel("birch", "birch", "column")
 
                 .createLang("en_us", new HashMap<>() {{
                     put("block.chassis.testblock", "Test Block");
                     put("item.chassis.testitem", "Test Item");
                     put("itemGroup.chassis.testgroup", "Test Group");
-                }});
+                }})
 
-        if (ConfigHandler.getBooleanOption(CHASSIS_CONFIGS, "hideResourcePack", false)) CHASSIS_RESOURCES.hide();
+                .createAdvancement(new Identifier("chassis", "testblock"), null, "Chassis Test", "Chassis is a library", new Identifier("chassis", "textures/block/testblock.png"), "challenge",
+                        true, true, false,
+                        null,
+                        "chassis_test", new Identifier("minecraft:inventory_changed"), new Identifier[]{new Identifier("chassis", "testblock")}, null);
     }
-*/
 }
--- src/main/java/evergoodteam/chassis/util/Reference.java
+++ src/main/java/evergoodteam/chassis/util/Reference.java
@@ -1,16 +1,16 @@
 package evergoodteam.chassis.util;
 
-/*
+
 import evergoodteam.chassis.configs.ConfigBase;
 import evergoodteam.chassis.objects.resourcepacks.ResourcePackBase;
-*/
 
 public class Reference {
 
     public static final String MODID = "chassis";
     public static final String CMI = StringUtils.capitalize(MODID);
-/*
+
     public static final ConfigBase CHASSIS_CONFIGS = new ConfigBase(MODID);
-    public static final ResourcePackBase CHASSIS_RESOURCES = new ResourcePackBase(CHASSIS_CONFIGS, MODID, "https://evergoodteam.github.io/utils/icons/chassisIcon.png", "53a074");
-*/
+    public static final ResourcePackBase CHASSIS_RESOURCES = new ResourcePackBase(CHASSIS_CONFIGS, MODID)
+            .addIcon("https://evergoodteam.github.io/utils/icons/chassisIcon.png")
+            .addColor("53a074");
 }
