3b47db2dd1682c436ed9c81703aa7448223c0d20
Libra <73041325+Librv@users.noreply.github.com>
2022-09-27 20:54:50 +0200

    BlockSettings.java: move transparent list

--- src/main/java/evergoodteam/chassis/client/ChassisClient.java
+++ src/main/java/evergoodteam/chassis/client/ChassisClient.java
@@ -1,5 +1,6 @@
 package evergoodteam.chassis.client;
 
+import evergoodteam.chassis.objects.blocks.BlockSettings;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -8,36 +9,20 @@ import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
 import org.slf4j.Logger;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static evergoodteam.chassis.util.Reference.CMI;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Environment(EnvType.CLIENT)
 public class ChassisClient implements ClientModInitializer {
 
-    private static final Set<Block> TRANSPARENT_BLOCKS = new HashSet<>();
     private static final Logger LOGGER = getLogger(CMI + "/Client");
 
     @Override
     public void onInitializeClient() {
         //LOGGER.info("Total transparent blocks: {}", getTransparentBlocks().size());
 
-        for (Block block : getTransparentBlocks()) {
+        for (Block block : BlockSettings.getTransparentBlocks()) {
             BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
         }
-    }
-
-    public static void addTransparentBlock(Block block) {
-        TRANSPARENT_BLOCKS.add(block);
-    }
-
-    public static void removeTransparentBlock(Block block) {
-        TRANSPARENT_BLOCKS.remove(block);
-    }
-
-    public static Set<Block> getTransparentBlocks() {
-        return TRANSPARENT_BLOCKS;
     }
 }
--- src/main/java/evergoodteam/chassis/objects/blocks/BlockSettings.java
+++ src/main/java/evergoodteam/chassis/objects/blocks/BlockSettings.java
@@ -1,12 +1,28 @@
 package evergoodteam.chassis.objects.blocks;
 
-import evergoodteam.chassis.client.ChassisClient;
 import net.minecraft.block.Block;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public interface BlockSettings {
 
+    Set<Block> TRANSPARENT_BLOCKS = new HashSet<>();
+
+    static void addTransparentBlock(Block block) {
+        TRANSPARENT_BLOCKS.add(block);
+    }
+
+    static void removeTransparentBlock(Block block) {
+        TRANSPARENT_BLOCKS.remove(block);
+    }
+
+    static Set<Block> getTransparentBlocks() {
+        return TRANSPARENT_BLOCKS;
+    }
+
     default Block setTransparent() {
-        if(this != null) ChassisClient.addTransparentBlock((Block) this);
+        if(this != null) addTransparentBlock((Block) this);
         return (Block) this;
     }
 }
