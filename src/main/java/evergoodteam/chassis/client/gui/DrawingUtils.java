5e61671daab319a49f217c447f1fa5f72e63c047
Libra <73041325+Librv@users.noreply.github.com>
2023-04-30 15:18:51 +0200

    DrawingUtils: support for gradient text

--- src/main/java/evergoodteam/chassis/client/gui/DrawingUtils.java
+++ src/main/java/evergoodteam/chassis/client/gui/DrawingUtils.java
@@ -1,12 +1,20 @@
-package evergoodteam.chassis.configs.widgets.util;
+package evergoodteam.chassis.client.gui;
 
+import evergoodteam.chassis.client.gui.text.GradientTextRenderer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+import java.util.List;
 
 @Environment(value = EnvType.CLIENT)
 public abstract class DrawingUtils extends DrawableHelper {
+
+    public static void gradientText(MatrixStack matrices, GradientTextRenderer gradientTextRenderer, Text text, List<Integer> points, int centerX, int y, int transparency) {
+        gradientTextRenderer.drawWithShadow(matrices, text, points, (float) (centerX - gradientTextRenderer.getWidth(text.asOrderedText()) / 2), (float) y, transparency);
+    }
 
     public void drawRectWithOutline(MatrixStack matrices, int x, int y, int width, int height, int color) {
         drawRectWithOutline(matrices, x, y, width, height, color, color);
