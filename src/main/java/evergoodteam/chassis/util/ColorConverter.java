55b504a84048170da8bd0c24e5be330f92e6016e
Libra <73041325+Librv@users.noreply.github.com>
2023-04-30 14:45:20 +0200

    Deprecate ColorConverter.java

--- src/main/java/evergoodteam/chassis/util/ColorConverter.java
+++ src/main/java/evergoodteam/chassis/util/ColorConverter.java
@@ -1,102 +1,33 @@
 package evergoodteam.chassis.util;
 
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
+import evergoodteam.chassis.util.gui.ColorUtils;
 
-import java.awt.*;
-
+/**
+ * @deprecated as of release 1.2.3, moved to {@link ColorUtils}
+ */
+@Deprecated
 public class ColorConverter {
 
-    public static Text coloredText(String text, String hexColor){
-        return Text.literal(text).setStyle(coloredStyle(hexColor));
-    }
-
-    public static Style coloredStyle(String hex){
-        return Style.EMPTY.withColor(getDecimalFromHex(hex));
-    }
-
-    //region Decimal
-
     /**
-     * Converts a Color object to a decimal color code
-     */
-    public static int getDecimalFromColor(Color color) {
-        return getDecimalFromRGB(getRGBFromColor(color));
-    }
-
-    /**
-     * Converts a hex color code to a decimal color code
+     * Convert a hex color code to a decimal color code
      *
      * @param hex color code WITHOUT '#'
      */
     public static int getDecimalFromHex(String hex) {
-        return Integer.parseInt(hex, 16);
-    }
-
-    public static int getDecimalFromRGB(Integer[] rgb) {
-        return (rgb[0] * 65536) + (rgb[1] * 256) + rgb[2];
+        return ColorUtils.getDecimalFromHex(hex);
     }
 
     /**
-     * Converts a RGB color code to a decimal color code
+     * Convert a RGB color code to a decimal color code
      */
     public static int getDecimalFromRGB(int r, int g, int b) {
-        return (r * 65536) + (g * 256) + b;
-    }
-    //endregion
-
-    //region RGB
-
-    /**
-     * Converts a decimal color code to a RGB color code
-     */
-    public static Integer[] getRGBFromDecimal(int decimal) {
-        return getRGBFromColor(new Color(decimal));
+        return ColorUtils.getDecimalFromRGB(r, g, b);
     }
 
     /**
-     * Converts a Color object to a RGB color code
-     */
-    public static Integer[] getRGBFromColor(Color color) {
-        int red = color.getRed();
-        int green = color.getGreen();
-        int blue = color.getBlue();
-
-        return new Integer[]{red, green, blue};
-    }
-    //endregion
-
-    //region Hex
-
-    /**
-     * Converts a Color object to a hex color code
-     */
-    public static String getHexFromColor(Color color) {
-        return getHexFromRGB(getRGBFromColor(color));
-    }
-
-    /**
-     * Converts a decimal color code to a hex color code
-     */
-    public static String getHexFromDecimal(int decimal) {
-        return getHexFromRGB(getRGBFromDecimal(decimal));
-    }
-
-    /**
-     * Converts a RGB color code to a hex color code
-     */
-    public static String getHexFromRGB(Integer[] rgb) {
-        return getHexFromRGB(rgb[0], rgb[1], rgb[2]);
-    }
-
-    /**
-     * Converts a RGB color code to a hex color code
+     * Convert a RGB color code to a hex color code
      */
     public static String getHexFromRGB(int r, int g, int b) {
-        String rs = Integer.toHexString(r * 256);
-        String gs = Integer.toHexString(g * 256);
-        String bs = Integer.toHexString(b * 256);
-        return rs + gs + bs;
+        return ColorUtils.getHexFromRGB(r, g, b);
     }
-    //endregion
 }
