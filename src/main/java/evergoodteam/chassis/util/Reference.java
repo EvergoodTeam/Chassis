d21d757a2a6971733068aaeca7df7dbaccf2a648
Libra <73041325+Librv@users.noreply.github.com>
2023-05-01 11:52:20 +0200

    Uncomment config declaration

--- src/main/java/evergoodteam/chassis/util/Reference.java
+++ src/main/java/evergoodteam/chassis/util/Reference.java
@@ -8,7 +8,7 @@ public class Reference {
     public static final String MODID = "chassis";
     public static final String CMI = StringUtils.capitalize(MODID);
 
-    //public static final ConfigBase CHASSIS_CONFIGS = new ConfigBase(MODID);
+    public static final ConfigBase CHASSIS_CONFIGS = new ConfigBase(MODID);
     /*public static final ResourcePackBase CHASSIS_RESOURCES = new ResourcePackBase(CHASSIS_CONFIGS, MODID)
             .setIcon("https://evergoodteam.github.io/utils/icons/chassisIcon.png")
             .setColor("53a074");*/
