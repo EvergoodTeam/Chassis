aa40abcbfa2c8c1f22575563469b702066820f95
Librv <73041325+Librv@users.noreply.github.com>
2023-07-12 10:02:56 +0200

    Added simple OverlayScreen to edit GUI elements

---
+++ src/main/java/evergoodteam/chassis/client/gui/screens/OverlayScreen.java
@@ -0,0 +1,49 @@
+package evergoodteam.chassis.client.gui.screens;
+
+import evergoodteam.chassis.client.gui.widgets.OverlayWidget;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+@Environment(value = EnvType.CLIENT)
+public class OverlayScreen extends Screen implements Element {
+
+    public OverlayWidget overlayWidget;
+
+    protected OverlayScreen(PlayerEntity player, OverlayWidget overlay) {
+        super(Text.literal("Overlay screen"));
+        this.overlayWidget = overlay;
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        this.renderBackground(context);
+        overlayWidget.render(context, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldCloseOnEsc() {
+        return false;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
+            overlayWidget.getUpdateCallback().onSave();
+            client.setScreen(null);
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+}
---
+++ src/main/java/evergoodteam/chassis/client/gui/widgets/OverlayWidget.java
@@ -0,0 +1,81 @@
+package evergoodteam.chassis.client.gui.widgets;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
+
+@Environment(value = EnvType.CLIENT)
+public class OverlayWidget extends AbstractWidget {
+
+    private final UpdateCallback updateCallback;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int color;
+    public int outlineColor;
+
+    public OverlayWidget(UpdateCallback callback, int x, int y, int width, int height, int color) {
+        this(callback, x, y, width, height, color, color);
+    }
+
+    public OverlayWidget(UpdateCallback callback, int x, int y, int width, int height, int color, int outlineColor) {
+        this.updateCallback = callback;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.outlineColor = outlineColor;
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        renderBackground(context, mouseX, mouseY, delta);
+    }
+
+    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta){
+        drawRectWithOutline(context, x, y, width, height, color, outlineColor);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        if (isLeftClick(button) && insideBounds(mouseX, mouseY)) {
+
+            // Drag the preview around, keeping the cursor at the same distance from previous XY
+            int distanceX = (int) (mouseX - x);
+            int distanceY = (int) (mouseY - y);
+            x = (int) (mouseX + deltaX - distanceX);
+            y = (int) (mouseY + deltaY - distanceY);
+
+            updateCallback.onUpdate();
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insideBounds(double mouseX, double mouseY) {
+        return mouseX >= (double) this.x && mouseY >= (double) this.y && mouseX < (double) (this.x + this.width) && mouseY < (double) (this.y + this.height);
+    }
+
+    @Environment(value = EnvType.CLIENT)
+    public interface UpdateCallback {
+        void onUpdate();
+
+        void onSave();
+    }
+
+    public UpdateCallback getUpdateCallback(){
+        return updateCallback;
+    }
+
+    @Override
+    public void setFocused(boolean focused) {
+    }
+
+    @Override
+    public boolean isFocused() {
+        return false;
+    }
+}
