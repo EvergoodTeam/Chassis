1dea9aee23d2fc2a5a12d0529e90cccfa7d0b1c5
Librv <73041325+Librv@users.noreply.github.com>
2023-07-12 09:51:26 +0200

    JsonUtils: moved JsonHandler methods, switched to JsonElement where possible

--- src/main/java/evergoodteam/chassis/util/JsonUtils.java
+++ src/main/java/evergoodteam/chassis/util/JsonUtils.java
@@ -1,9 +1,6 @@
 package evergoodteam.chassis.util;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import net.minecraft.util.Identifier;
+import com.google.gson.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -15,10 +12,32 @@ import java.nio.file.Path;
 import static evergoodteam.chassis.util.Reference.CMI;
 import static org.slf4j.LoggerFactory.getLogger;
 
-// TODO: JsonElement after deprecation
 public class JsonUtils {
 
-    private static final Logger LOGGER = getLogger(CMI + "/U/Json");
+    private static final Logger LOGGER = getLogger(CMI + "/H/Json");
+    private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+
+    /**
+     * After translating the string to a {@link JsonElement}, writes it to the .json file identified by the path
+     */
+    public static void writeToJson(String json, @NotNull Path path) {
+        writeToJson(toJson(json), path);
+    }
+
+    /**
+     * Writes the provided {@link JsonObject} to the .json file identified by the path
+     */
+    public static void writeToJson(JsonElement json, @NotNull Path path) {
+        if (isJsonFile(path)) {
+            //LOGGER.info("Attempting to write to path {}", path);
+            if (json.getAsJsonObject().size() != 0) {
+                String jsonString = GSON.toJson(json);
+                FileUtils.writeToFile(jsonString, path);
+            } else {
+                LOGGER.warn("Provided Json is empty, cannot write it to {}", path);
+            }
+        }
+    }
 
     /**
      * Checks whether the specified file is a .json file
@@ -33,10 +52,9 @@ public class JsonUtils {
      * @param file path of a valid .json file
      * @return {@link JsonObject} with all the contents of the specified .json file
      */
-    public static @Nullable JsonObject toJsonObject(@NotNull Path file) {
+    public static @Nullable JsonElement toJson(@NotNull Path file) {
         try {
-            JsonElement jsonElement = JsonParser.parseReader(new FileReader(file.toString()));
-            return jsonElement.getAsJsonObject();
+            return JsonParser.parseReader(new FileReader(file.toString()));
         } catch (FileNotFoundException e) {
             LOGGER.error("File not found", e);
         }
@@ -49,28 +67,8 @@ public class JsonUtils {
      *
      * @param json valid json transcript
      */
-    public static JsonObject toJsonObject(@NotNull String json) {
-        return JsonParser.parseString(json).getAsJsonObject();
-    }
-
-    /**
-     * Translates a {@link JsonObject} to a string
-     *
-     * @param json valid {@link JsonObject}
-     */
-    public static String toString(@NotNull JsonObject json) {
-        return json.toString();
-    }
-
-    /**
-     * Adds the specified value if not null to the provided {@link JsonObject}
-     *
-     * @param parent {@link JsonObject} to add to
-     * @param name   name of the property
-     * @param value  value of the property
-     */
-    public static void addPropertyIfNotNull(JsonObject parent, String name, Identifier value) {
-        if (value != null) parent.addProperty(name, value.toString());
+    public static JsonElement toJson(@NotNull String json) {
+        return JsonParser.parseString(json);
     }
 
     /**
