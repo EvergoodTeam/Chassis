c805ace9eca688bb570e890c1e0f72a61992a235
Librv <73041325+Librv@users.noreply.github.com>
2023-07-12 09:52:14 +0200

    FileUtils: moved FileHandler methods

--- src/main/java/evergoodteam/chassis/util/FileUtils.java
+++ src/main/java/evergoodteam/chassis/util/FileUtils.java
@@ -1,10 +1,16 @@
 package evergoodteam.chassis.util;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import static evergoodteam.chassis.util.Reference.CMI;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -22,11 +28,102 @@ public class FileUtils {
         return false;
     }
 
+    /**
+     * Returns the lines from the file as a {@link List}
+     */
+    public static List<String> toList(Path path) {
+        return Lists.newArrayList(toString(path).split("\\r?\\n"));
+    }
+
     public static String toString(Path path){
         try {
-            return Files.readString(path).strip();
+            return Files.readString(path, Charsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void emptyFile(Path path) {
+        emptyFile(path.toFile());
+    }
+
+    public static void emptyFile(File file) {
+        if (file.exists()) {
+            try {
+                new FileWriter(file, false).close();
+                //LOGGER.info("Emptied file at {}", file.getPath());
+            } catch (IOException e) {
+                LOGGER.error("Error on emptying file at {}", file.getPath(), e);
+            }
+        } else {
+            LOGGER.warn("Provided path is not valid: {}", file.getPath());
+        }
+    }
+
+    /**
+     * Writes the provided string to the file at the specified path
+     */
+    public static void writeToFile(String text, Path path) {
+        //LOGGER.info("Attempting to write to path {}", path);
+        if (Files.exists(path)) {
+            try (Writer fileWriter = new FileWriter(path.toFile())) {
+                fileWriter.write(text);
+            } catch (IOException e) {
+                LOGGER.error("Error on writing to file at {}", path, e);
+            }
+        } else {
+            LOGGER.warn("Provided path is not valid: {}", path);
+        }
+    }
+
+    /**
+     * Creates a json file at the specified path
+     *
+     * @param path NOTE: ".json" extension is not required
+     */
+    public static void createJsonFile(Path path) {
+        createFile(path, ".json");
+    }
+
+    /**
+     * Creates a file only if one with the same Path doesn't exist already
+     *
+     * @param path      file path
+     * @param extension e.g. ".txt", ".json"
+     */
+    public static void createFile(Path path, String extension) {
+        createFile(StringUtils.addExtension(path, extension));
+    }
+
+    /**
+     * Creates a file only if one with the same path doesn't exist already
+     *
+     * @param path NOTE: must include the file extension
+     */
+    public static void createFile(Path path) {
+        if (!Files.exists(path)) {
+            try {
+                Files.createFile(path);
+            } catch (IOException e) {
+                LOGGER.error("Error on creating File at {}", path, e);
+            }
+        }
+        else LOGGER.warn("Unable to create file as it already exists at {}", path);
+    }
+
+    /**
+     * Deletes a file at the specified path
+     *
+     * @param path path of your file
+     */
+    public static void delete(Path path) {
+        try {
+            if (Files.exists(path) && !Files.isDirectory(path)) {
+                org.apache.commons.io.FileUtils.delete(path.toFile());
+                LOGGER.debug("Deleted file at {}", path);
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error on deleting file at {}", path, e);
         }
     }
 }
