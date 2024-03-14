1510a01c5e69171fd07b73c45d3562a5bb9f048b
Librv <73041325+Librv@users.noreply.github.com>
2024-03-14 20:43:21 +0100

    Fix build naming, comment tests

--- src/main/java/evergoodteam/chassis/util/Reference.java
+++ src/main/java/evergoodteam/chassis/util/Reference.java
@@ -1,16 +1,17 @@
 package evergoodteam.chassis.util;
-
+/*
 import evergoodteam.chassis.config.ConfigBase;
 import evergoodteam.chassis.common.resourcepack.ResourcePackBase;
 import net.minecraft.util.Identifier;
-
+*/
 public class Reference {
 
     public static final String MODID = "chassis";
     public static final String CMI = StringUtils.capitalize(MODID);
-
+    /*
     public static final ConfigBase CHASSIS_CONFIGS = new ConfigBase(new Identifier("chassis", "chassis"));
     public static final ResourcePackBase CHASSIS_RESOURCES = new ResourcePackBase(CHASSIS_CONFIGS, MODID)
             .setIcon("https://evergoodteam.github.io/utils/icons/chassisIcon.png")
             .setColor("53a074");
+    */
 }
